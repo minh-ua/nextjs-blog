8aa3c679a84dec585f618a639d243f3d649a30d2
Minh Nguyen <minhrau92@gmail.com>
2022-09-26 15:31:17 +0900

    Test

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Software Engineer]</p>
+        <p>[Senior Software Engineer]</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
